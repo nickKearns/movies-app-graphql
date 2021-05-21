eaef55593fd17ffb46c23611eebbc4a2203148e4
Mohammad Azam <azamsharp@gmail.com>
2021-05-21 10:00:34 -0500

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Mohammad Azam on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
